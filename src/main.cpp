6793c22b0ad35d53f22e5c27814c8b21a4f03866
Alexandru Ica <alexandruica703@gmail.com>
2020-08-12 12:44:39 +0300

    Write a simple test for `gfx::window`

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
+#include "gfx/graphics.hpp"
+#include "gfx/window.hpp"
 #include "log/log.hpp"
 
 auto main(int, char*[]) noexcept -> int
 {
-    INFO("Hello {}", "there");
+    gfx::window wnd{ "SortVisualizer" };
+
+    wnd.on_key_press([](gfx::key_event const ev) {
+        if(ev == gfx::key_event::right) {
+            TRACE("RIGHT arrow pressed");
+        }
+        else if(ev == gfx::key_event::space) {
+            TRACE("SPACE key pressed");
+        }
+    });
+
+    gfx::set_clear_color(gfx::color{});
+
+    while(!wnd.should_close()) {
+        wnd.handle_events();
+
+        gfx::clear();
+
+        wnd.swap_buffers();
+    }
 }
