8848b3aeae55ed85459580e07d426374b63fe992
Alexandru Ica <alexandruica703@gmail.com>
2020-08-13 17:22:38 +0300

    Skeleton for handling key presses for pausing/resuming sorting

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,9 @@
 #include "gfx/window.hpp"
 #include "log/log.hpp"
 
+#include <chrono>
 #include <numeric>
-#include <random>
+#include <thread>
 
 [[nodiscard]] auto generate_data(core::element_t const count) -> std::vector<core::element_t>
 {
@@ -24,12 +25,18 @@ auto main(int, char*[]) noexcept -> int
 {
     gfx::window wnd{ "SortVisualizer" };
 
-    wnd.on_key_press([](gfx::key_event const ev) {
+    bool process_next_event = false;
+    bool pause_after_iteration = false;
+
+    wnd.on_key_press([&process_next_event, &pause_after_iteration](gfx::key_event const ev) {
         if(ev == gfx::key_event::right) {
             TRACE("RIGHT arrow pressed");
+            process_next_event = true;
+            pause_after_iteration = true;
         }
         else if(ev == gfx::key_event::space) {
             TRACE("SPACE key pressed");
+            process_next_event = !process_next_event;
         }
     });
 
@@ -39,12 +46,59 @@ auto main(int, char*[]) noexcept -> int
     gfx::sort_view_config cfg{};
     gfx::sort_view view{ cfg, data };
 
+    core::array input{ data };
+    std::thread sort_thread{ [&input] { core::algorithm::bubble_sort(input); } };
+    auto& ev = core::event_manager::instance();
+
+    using namespace std::chrono;
+    using namespace std::chrono_literals;
+
+    constexpr auto delay = 150ms;
+    auto start = steady_clock::now();
+
     while(!wnd.should_close()) {
         wnd.handle_events();
+
+        auto end = steady_clock::now();
+        auto const duration = end - start;
+
+        if(process_next_event && duration >= delay && !ev.empty()) {
+            start = end;
+            auto const event = ev.pop();
+
+            switch(event.type) {
+            case core::event_type::access: {
+                TRACE("[Consumer] Accessed #{}", event.i);
+                break;
+            }
+            case core::event_type::compare: {
+                TRACE("[Consumer] Compared #{} with #{}", event.i, event.j);
+                break;
+            }
+            case core::event_type::modify: {
+                TRACE("[Consumer] Modified #{} with #{}", event.i, event.j);
+                break;
+            }
+            case core::event_type::swap: {
+                TRACE("[Consumer] Swapped #{} with #{}", event.i, event.j);
+                break;
+            }
+            default: {
+                break;
+            }
+            }
+        }
+
+        if(pause_after_iteration) {
+            process_next_event = false;
+            pause_after_iteration = false;
+        }
 
         gfx::clear();
         view.draw();
 
         wnd.swap_buffers();
     }
+
+    sort_thread.join();
 }
