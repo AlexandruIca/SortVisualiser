d26f3678982cdc919166cf972842855e1b6f8789
Alexandru Ica <alexandruica703@gmail.com>
2020-08-17 16:48:49 +0300

    Improve the bubble sort algorithm

--- src/algorithm/algorithm.cpp
+++ src/algorithm/algorithm.cpp
@@ -5,18 +5,25 @@ namespace core::algorithm {
 
 auto bubble_sort(core::array& data) -> void
 {
+    int sorted_offset = 0;
     bool is_sorted = false;
 
     do {
         is_sorted = true;
 
-        for(int i = 1; i < data.isize(); ++i) {
+        for(int i = 1; i < data.isize() - sorted_offset; ++i) {
             if(data[i - 1] > data[i]) {
                 data.swap_at(i - 1, i);
                 is_sorted = false;
             }
         }
+
+        if(!is_sorted) {
+            ++sorted_offset;
+        }
     } while(!is_sorted);
+
+    data.end();
 }
 
 } // namespace core::algorithm
